aee4bf6dfe0ef55d16397dfe2192cfb1fceed8dc
Liethan Seth Velasco <liethanseth@rogers.com>
2023-02-12 18:31:30 -0500

    Updating with make.

--- task1/hello.mod.c
+++ task1/hello.mod.c
@@ -27,7 +27,7 @@ MODULE_INFO(retpoline, "Y");
 
 static const struct modversion_info ____versions[]
 __used __section("__versions") = {
-	{ 0xf704969, "module_layout" },
+	{ 0x32e21920, "module_layout" },
 	{ 0x5b8239ca, "__x86_return_thunk" },
 	{ 0x92997ed8, "_printk" },
 	{ 0xbdfb6dbb, "__fentry__" },
